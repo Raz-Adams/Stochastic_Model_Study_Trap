c9b5e313a47712945c385f1e94134ff269312310
Razaak Adams <natadd156@gmail.com>
2025-07-15 20:24:43 +0000

    Main .py file for drunkard's walk

---
+++ Drunkard's_walk/Drunkard_walk_project.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Simulation parameters
+num_steps = 100
+
+# Creating random steps
+# Each choice should be either 1 or -1 (i.e forward or backward)
+
+steps = np.random.choice([1,-1], size = num_steps)
+
+#Calculating the path
+#The position at any point is the sum of the random 1000 steps taken
+#np.insert(0,0) we want to have 0 as the starting position not 1 (which would have meant he has 
+#already taken a step)
+positions = np.cumsum(np.insert(steps,0,0))
+
+#Visualizing our code
+plt.figure(figsize=(10,6))   #create graph
+plt.plot(positions)             #plot positions
+
+#Adding Labels to Plot
+plt.title("1D Random Walk (Drunkard's Path)")
+plt.xlabel("Number of Steps")
+plt.ylabel("Position from Start")
+plt.grid(True)
+
+plt.show()
+
